3d366b5fb9bdc93918440f5cae0ebcdd0f3e0918
arshadmohd45 <arshadmohd2522@gmail.com>
2025-11-13 23:28:18 +0530

    Create PersonMain.java

---
+++ PersonMain.java
@@ -0,0 +1,19 @@
+package com.avn.tns.oopsdemo;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+	
+		Person p=new Person();
+		p.setName("SRIRAM");
+		p.setPid(99999);
+		p.setAge(21);
+		
+		System.out.println(p.getName());
+		System.out.println(p.getPid());
+		System.out.println(p.getAge());
+		
+
+	}
+
+}
