d441a5fa975f2f94690980a3d4841b58a111fbd6
arshadmohd45 <arshadmohd2522@gmail.com>
2025-11-13 23:23:36 +0530

    datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,12 @@
+class DataTypes{
+    public static void main(String args[]){
+        int a=10;
+        long l=100l;
+        float f=10.4f;
+        double d=10.09;
+        byte b=10;
+        short s=20000;
+        boolean bo=true;
+        System.out.println("DataTypes:"+" "+a+" "+l+" "+f+" "+d+" "+b+" "+s+" "+bo);
+    }
+}
