6bc93f83c5c5c872ed12fd8484048adc3fea87ed
arshadmohd45 <arshadmohd2522@gmail.com>
2025-11-13 23:32:53 +0530

    AccessDemo.java

---
+++ AccessDemo.java
@@ -0,0 +1,13 @@
+ackage com.avn.tns.accessdemo;
+
+public class AccessDemo {
+
+	public String name;
+    private int phoneno;
+    protected String id;
+    String branch;
+     public void display(){
+        System.out.println("hello world");
+	}
+
+}
